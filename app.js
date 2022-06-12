40434000c045c8607d5c63a09e2a47181382339e
MeryAmun <chebesiawah@gmail.com>
2022-06-12 13:37:31 +0100

    '6'

--- app.js
+++ app.js
@@ -626,6 +626,22 @@ return sotrtedArr
 }
 
 console.log(nonMutatingSort(globalArray));
+
+/* 
+Use the split method inside the splitify function to split str into an array of words. The function should return the array. Note that the words are not always separated by spaces, and the array should not contain punctuation.
+
+
+*/
+
+function splitify(str) {
+    // Only change code below this line
+  
+  const splitStr = str.split(/\W/)
+  return splitStr
+    // Only change code above this line
+  }
+  
+  console.log(splitify("Hello World,I-am code"));
 /* 
 
  Exercise seven
