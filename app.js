2b902706a8fa2fdb9747e4fbef29bab3c34a06c8
MeryAmun <chebesiawah@gmail.com>
2022-06-13 01:06:22 +0100

    '20'

--- app.js
+++ app.js
@@ -737,3 +737,33 @@ console.log(newStr.join(' '))
  
   }
    alphabetPosition("The sunset sets at twelve o'clock.");
+
+
+   /* 
+   Use the every Method to Check that Every Element in an Array Meets a Criteria
+The every method works with arrays to check if every element passes a particular test. It returns a Boolean value - true if all values meet the criteria, false if not.
+   
+Use the every method inside the checkPositive function to check if every element in arr is positive. The function should return a Boolean value.
+   */
+
+
+  
+function checkPositive(arr) {
+    // Only change code below this line
+    let result = []
+    for(let i = 0; i < arr.length; i++){
+      let num = arr[i]
+  result = arr.every(function(num){
+    if( num >= 0 ){
+      return true
+    }else{
+   return false
+    }
+   
+  })
+   }
+  return result
+    // Only change code above this line
+  }
+  
+  console.log(checkPositive([1, 2, 3, -4, 5]));
