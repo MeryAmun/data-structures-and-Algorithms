f42d36837bdf30626ac0c4959eb9de2afc04cbff
MeryAmun <chebesiawah@gmail.com>
2022-06-13 01:30:24 +0100

    '24'

--- app.js
+++ app.js
@@ -775,12 +775,36 @@ function checkPositive(arr) {
 
 For example, the following code would check if any element in the numbers array is less than 10:
   array.some(function(value, index, arr), this)
+
+
+  Use the some method inside the checkPositive function to check if any element in arr is positive. The function should return a Boolean value.
   
   */
 
+  //example
   const numbers = [10, 50, 8, 220, 110, 11];
 
   numbers.some(function(currentValue) {
     return currentValue < 10;
   });
   //The some method would return true.
+
+
+  //exerces 24
+
+  //Use the some method inside the checkPositive function to check if any element in arr is positive. The function should return a Boolean value.
+  function checkPositive(arr) {
+    // Only change code below this line
+    let booleanValue;
+  arr.some((currentValue) => {
+    if(currentValue > 0 ){ 
+      booleanValue = true
+      }else{
+         booleanValue = false
+      }
+  })
+  return booleanValue
+    // Only change code above this line
+  }
+  
+  checkPositive([1, 2, 3, -4, 5]);
