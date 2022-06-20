da6caec12374a2b9f7fed698a6ecfe93c4b1c701
MeryAmun <chebesiawah@gmail.com>
2022-06-20 06:38:24 +0100

    '46'

--- app.js
+++ app.js
@@ -1379,3 +1379,26 @@ function smallestCommons(arr) {
 }
 
 smallestCommons([1, 5]);
+
+/*
+Drop it
+Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+
+Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+
+*/
+
+//solution 1
+function dropElements(arr, func) {
+  let l = arr.length;
+  for (let i = 0; i < l; i++) {
+    if (func(arr[0])) {
+      break;
+    } else {
+      arr.shift();
+    }
+  }
+  return arr;
+}
+
+dropElements([1, 2, 3], function(n) {return n < 3; });
