93d0007a33ff5da8094b2d387fb6816a2715f462
MeryAmun <chebesiawah@gmail.com>
2022-06-20 05:31:53 +0100

    '45'

--- app.js
+++ app.js
@@ -1350,3 +1350,32 @@ function getPrimeFactors(num) {
 }
 
 smallestCommons([1, 5]);
+
+//solution two
+function smallestCommons(arr) {
+
+  const [min, max] = arr.sort((a, b) => a - b);
+  console.log(min, max)
+  const numberDivisors = max - min + 1;
+  console.log(numberDivisors)
+  let upperBound = 1;
+  for (let i = min; i <= max; i++) {
+    upperBound *= i;
+  }
+  // Test all multiples of 'max'
+  for (let multiple = max; multiple <= upperBound; multiple += max) {
+    // Check if every value in range divides 'multiple'
+    let divisorCount = 0;
+    for (let i = min; i <= max; i++) {
+      // Count divisors
+      if (multiple % i === 0) {
+        divisorCount += 1;
+      }
+    }
+    if (divisorCount === numberDivisors) {
+      return multiple;
+    }
+  }
+}
+
+smallestCommons([1, 5]);
