0cada23e9f8f4ede1f26e210bd00e6ba81d33a9b
MeryAmun <chebesiawah@gmail.com>
2022-06-25 01:44:14 +0100

    '60'

--- app.js
+++ app.js
@@ -1802,3 +1802,23 @@ endIndex = alphEnd.indexOf(letterToDecode);
 }
 
 rot13("SERR CVMMN!");
+
+/* 
+Telephone Number Validator
+Return true if the passed string looks like a valid US phone number.
+
+The user may fill out the form field any way they choose as long as it has the format of a valid US number. The following are examples of valid formats for US numbers (refer to the tests below for other variants):
+
+*/
+function telephoneCheck(str) {
+  let result ;
+ let regex = /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/;
+   
+  result = regex.test(str);
+  
+  // console.log(str.match(regex))
+console.log(result)
+  return result;
+}
+
+telephoneCheck("555-555-5555");
