de7497bbc2d8762a4dfb9a33b71a328df65b6d49
MeryAmun <chebesiawah@gmail.com>
2022-06-28 18:54:28 +0100

    '65'

--- app.js
+++ app.js
@@ -946,6 +946,15 @@ function diffArray(arr1, arr2) {
   
     return newArr;
   }
+  //solution two
+  function diffArray(arr1, arr2) {
+    let newArr = [];
+   newArr = arr1.concat(arr2)
+   .filter((item) => !arr1.includes(item) || !arr2.includes(item))
+     return newArr;
+   }
+   
+   diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
   
   console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]))
 
